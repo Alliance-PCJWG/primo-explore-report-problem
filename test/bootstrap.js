5414c27322afda4f721dc57ead5c72e2e68bc74c
Jeremy McWilliams <jeremym@lclark.edu>
2019-06-05 14:51:48 -0700

    issue 16

--- test/bootstrap.js
+++
@@ -1,3 +0,0 @@
-var app = angular.module('viewCustom', ['reportProblem'])
-
-app.component('prmActionListAfter', {template: '<oca-report-problem report-url="http://my.library.edu/reportproblem.php?" />'})
